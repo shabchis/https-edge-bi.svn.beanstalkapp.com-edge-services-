2aa0cab9ff0c7cca2992a7de5a40791f5f37c9ef
alonyaari <alonyaari@738af68e-eb0f-4933-9496-cee620179486>
2011-04-03 12:41:47 +0000

    adding Facebook

---
+++ Facebook/Edge.Services.Facebook.AdsApi/trunk/InitializerService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Edge.Core.Services;
+using Edge.Data.Pipeline;
+
+namespace Edge.Services.Facebook.AdsApi
+{
+	public class InitializerService: PipelineService
+	{
+		protected override ServiceOutcome DoWork()
+		{
+			// Create a new delivery
+			this.Delivery = new Delivery(this.Instance.InstanceID)
+			{
+				TargetPeriod = this.TargetPeriod
+			};
+
+			// TODO: add delivery files
+			this.Delivery.Files.Add(new DeliveryFile()
+			{
+				Name = "myFile",
+				ReaderType = typeof(XPathRowReader<PpcDataUnit>)
+			});
+
+
+			this.Delivery.Save();
+
+			return ServiceOutcome.Success;
+		}
+	}
+}
---
+++ Facebook/Edge.Services.Facebook.AdsApi/trunk/ProcessorService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Edge.Data.Pipeline;
+
+namespace Edge.Services.Facebook.AdsApi
+{
+	public class ProcessorService:PipelineService
+	{
+		protected override Core.Services.ServiceOutcome DoWork()
+		{
+			
+		}
+	}
+}
