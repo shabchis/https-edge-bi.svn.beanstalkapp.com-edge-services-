83457c79e3ababfec386783f122865c0d5baa6b2
doronassayas <doronassayas@738af68e-eb0f-4933-9496-cee620179486>
2011-04-03 11:17:48 +0000

    Adding AdCenter services

---
+++ Microsoft/Edge.Services.Microsoft.AdCenter/trunk/Services/AdCenterInitializerService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Edge.Data.Pipeline;
+using Edge.Core.Services;
+
+namespace Edge.Services.Microsoft.AdCenter
+{
+	public class AdCenterInitializerService : AdCenterInitializerBase
+	{
+		protected override ServiceOutcome DoWork()
+		{
+			// Create a new delivery
+			this.Delivery = new Delivery(this.Instance.InstanceID)
+			{
+				TargetPeriod = this.TargetPeriod
+			};
+
+			// AccountID as parameter for entire delivery
+			this.Delivery.Parameters["AccountID"] = this.Instance.AccountID;
+
+			// Both keyword and ad performance reports are needed
+			this.Delivery.Files.Add(new DeliveryFile()
+			{
+				Name = "KeywordPerformanceReport",
+				SourceUrl = SubmitReportRequest(NewKeywordPerformanceReportRequest()),
+				ReaderType = typeof(KeywordPerformanceReportReader)
+			});
+			ReportProgress(0.49); // progress: 49%
+
+			this.Delivery.Files.Add(new DeliveryFile()
+			{
+				Name = "AdPerformanceReport",
+				SourceUrl = SubmitReportRequest(NewAdPerformanceReportRequest()),
+				ReaderType = typeof(AdPerformanceReportReader)
+			});
+			ReportProgress(0.98); // progress: 98%
+
+			// Save with success
+			this.Delivery.Save();
+
+			return ServiceOutcome.Success;
+		}
+	}
+}
---
+++ Microsoft/Edge.Services.Microsoft.AdCenter/trunk/Services/AdCenterProcessorService.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Edge.Core.Services;
+using System.ServiceModel;
+using Edge.Data.Pipeline;
+using System.IO;
+using System.IO.Compression;
+using System.Xml;
+using System.Reflection;
+using GotDotNet.XPath;
+using Edge.Core.Data;
+using Edge.Data.Pipeline.GkManager;
+using WS = Edge.Services.Microsoft.AdCenter.ServiceReferences.V7.ReportingService;
+using Edge.Core;
+
+
+namespace Edge.Services.Microsoft.AdCenter
+{
+    public class AdCenterProcessorService : PipelineService
+    {
+        protected override ServiceOutcome DoWork()
+        {
+			// TODO: add checks for delivery state
+
+			// Create the readers
+			var keywordReportReader = (XPathRowReader<PpcDataUnit>) this.Delivery.Files["KeywordPerformance"].CreateReader();
+			var adReportReader = (XPathRowReader<PpcDataUnit>)this.Delivery.Files["AdPerformance"].CreateReader();
+
+			keywordReportReader.OnNextRowRequired = innerReader =>
+			{
+				SettingsCollection xmlValues = GetElementValues(innerReader);
+				return UnitFromValues(xmlValues);
+			};
+
+
+			// Start a transaction
+			try
+			{
+				using (DataManager.Current.OpenConnection())
+				{
+					DataManager.Current.StartTransaction();
+
+					while (keywordReportReader.Read())
+					{
+					}
+
+					DataManager.Current.CommitTransaction();
+				}
+			}
+			finally
+			{
+				keywordReportReader.Dispose();
+				adReportReader.Dispose();
+			}
+
+            return ServiceOutcome.Success;
+        }
+
+		private PpcDataUnit UnitFromValues(SettingsCollection values)
+		{
+			var data = new PpcDataUnit();
+
+			//..................
+			// IDENTITIES
+
+			// AccountID
+			data.AccountID = Instance.AccountID;
+
+			// Raw values (legacy DB schema)
+			data.Extra.Account_OriginalID				= values[WS.KeywordPerformanceReportColumn.AccountNumber.ToString()];
+			data.Extra.Campaign_Name					= values[WS.KeywordPerformanceReportColumn.CampaignName.ToString()];
+			data.Extra.Campaign_OriginalID				= values[WS.KeywordPerformanceReportColumn.CampaignId.ToString()];
+			data.Extra.Adgroup_Name						= values[WS.KeywordPerformanceReportColumn.AdGroupName.ToString()];
+			data.Extra.Adgroup_OriginalID				= values[WS.KeywordPerformanceReportColumn.AdGroupId.ToString()];
+			data.Extra.Keyword_Text						= values[WS.KeywordPerformanceReportColumn.Keyword.ToString()];
+			data.Extra.Keyword_OriginalID				= values[WS.KeywordPerformanceReportColumn.KeywordId.ToString()];
+			data.Extra.Creative_OriginalID				= values[WS.KeywordPerformanceReportColumn.AdId.ToString()];
+			data.Extra.AdgroupKeyword_DestUrl			= values[WS.KeywordPerformanceReportColumn.DestinationUrl.ToString()];
+			data.Extra.AdgroupKeyword_OriginalMatchType	= values[WS.KeywordPerformanceReportColumn.MatchType.ToString()];
+			data.Extra.Currency_Code					= values[WS.KeywordPerformanceReportColumn.CurrencyCode.ToString()];
+
+			// Match type
+			string rawMatchType;
+			MatchType matchType;
+			if (values.TryGetValue(data.Extra.AdgroupKeyword_OriginalMatchType, out rawMatchType))
+				Enum.TryParse<MatchType>(rawMatchType, out matchType);
+
+			// campaign
+			data.CampaignGK = GkManager.GetCampaignGK(
+				Instance.AccountID,
+				Delivery.ChannelID,
+				data.Extra.Campaign_Name,
+				data.Extra.Campaign_OriginalID
+				);
+
+			// adgroup
+			data.AdgroupGK = GkManager.GetAdgroupGK(
+				Instance.AccountID,
+				Delivery.ChannelID,
+				data.CampaignGK.Value,
+				data.Extra.Adgroup_Name,
+				data.Extra.Adgroup_OriginalID
+				);
+
+			// keyword
+			data.KeywordGK = GkManager.GetKeywordGK(
+				Instance.AccountID,
+				data.Extra.Keyword_Text
+				);
+
+			// adgroup keyword
+			data.AdgroupKeywordGK = GkManager.GetAdgroupKeywordGK(
+				Instance.AccountID,
+				Delivery.ChannelID,
+				data.CampaignGK.Value,
+				data.AdgroupGK.Value,
+				data.KeywordGK.Value,
+				matchType,
+				data.Extra.AdgroupKeyword_DestUrl,
+				null
+				);
+
+			// currency conversion
+			data.CurrencyID = Currency.GetByCode(data.Extra.Currency_Code).ID;
+
+			//..................
+			// METRICS
+
+			data.Impressions			= Int32.Parse(values[WS.KeywordPerformanceReportColumn.Impressions.ToString()]);
+			data.Clicks					= Int32.Parse(values[WS.KeywordPerformanceReportColumn.Clicks.ToString()]);
+			data.Cost					= Double.Parse(values[WS.KeywordPerformanceReportColumn.Spend.ToString()]);
+			data.AveragePosition		= Double.Parse(values[WS.KeywordPerformanceReportColumn.AveragePosition.ToString()]);
+			data.Conversions			= Int32.Parse(values[WS.KeywordPerformanceReportColumn.Conversions.ToString()]);
+
+			return data;
+		}
+
+		private SettingsCollection GetElementValues(XmlReader reader)
+		{
+			var dict = new SettingsCollection();
+			using (var r = reader.ReadSubtree())
+			{
+				while (r.Read())
+				{
+					if (r.NodeType == XmlNodeType.Element)
+						dict[r.Name] = r.ReadElementContentAsString();
+				}
+			}
+			return dict;
+		}
+    }
+}
