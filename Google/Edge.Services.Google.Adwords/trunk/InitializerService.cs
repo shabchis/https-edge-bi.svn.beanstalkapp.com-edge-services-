96539a3287b5790126a2a4b9fb6ae5ca3f372f86
sbarchen <sbarchen@738af68e-eb0f-4933-9496-cee620179486>
2011-04-10 12:27:48 +0000

    Adding Adwords service

---
+++ Google/Edge.Services.Google.Adwords/trunk/InitializerService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Edge.Data.Pipeline;
+
+namespace Edge.Services.Google.Adwords
+{
+    public class InitializerService: PipelineService
+    {
+        protected override Core.Services.ServiceOutcome DoPipelineWork()
+        {
+            this.Delivery = new Delivery(Instance.InstanceID);
+            this.Delivery.TargetPeriod = this.TargetPeriod;
+
+            this.Delivery.Parameters["AccountID"] = Instance.AccountID;
+
+            //Instance.Configuration.Options["Adwords.Email"]
+
+            this.Delivery.Files.Add(new DeliveryFile()
+            {
+                Name = "AdwordsCreativeReport",
+                SourceUrl = url // TODO: get from API
+            });
+
+            this.Delivery.Save();
+
+            return Core.Services.ServiceOutcome.Success;
+
+        }
+    }
+}
