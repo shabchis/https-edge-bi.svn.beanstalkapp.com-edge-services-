6082eb26d7f51d43b50167ed1954c600ff26d093
doronassayas <doronassayas@738af68e-eb0f-4933-9496-cee620179486>
2011-07-27 14:14:28 +0000

    Splitting validations into separate project

---
+++ Edge.Services.AdMetrics/trunk/Importing/AdMetricsImportMappingsConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Reflection;
+
+namespace Edge.Services.AdMetrics.Configuration
+{
+	public class ImportMappingsConfiguration
+	{
+	}
+
+	public class ImportMapping
+	{
+		public List<MappingSource> MappingSources;
+		public MappingTarget MappingTarget;
+	}
+
+	public class MappingSource
+	{
+		public string Name;
+		public string Field;
+		public Regex Regex;
+	}
+
+	public class MappingTarget
+	{
+		public MappingTargetPath Property;
+		public Type NewObjectType;
+	}
+
+	public class MappingTargetPath
+	{
+		public Type ObjectType;
+		public FieldInfo FieldInfo;
+	}
+
+	public class MappingPath
+	{
+	}
+}
