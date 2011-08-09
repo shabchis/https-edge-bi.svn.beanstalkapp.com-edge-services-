a2acc26bc8c8b6f909d2e132da538171bec474e8
doronassayas <doronassayas@738af68e-eb0f-4933-9496-cee620179486>
2011-08-09 12:37:20 +0000

    Tweaks and examples for validations/checksums (for Shay)

---
+++ Edge.Services.AdMetrics.Validations/trunk/DeliveryOltpChecksumService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Edge.Data.Pipeline;
+using Edge.Data.Pipeline.Services;
+
+namespace Edge.Services.AdMetrics.Validations
+{
+	public class DeliveryOltpChecksumService: ValidationService
+	{
+		protected override IEnumerable<ValidationResult> Validate()
+		{
+			Delivery d = Delivery.GetByTargetPeriod(channelID, accountID, start, end);
+
+
+			Delivery[] deliveriesToCheck;
+
+			foreach (Delivery delivery in deliveriesToCheck)
+			{
+				yield return new ValidationResult()
+				{
+
+				};
+			}
+		}
+	}
+}
