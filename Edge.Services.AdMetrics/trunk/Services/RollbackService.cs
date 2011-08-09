24301a4340742d3dc210d70d0c63222e72f4a955
doronassayas <doronassayas@738af68e-eb0f-4933-9496-cee620179486>
2011-08-09 12:02:26 +0000

    Pipeline upgrade for validation services.

---
+++ Edge.Services.AdMetrics/trunk/Services/RollbackService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Edge.Data.Pipeline.Services;
+
+namespace Edge.Services.AdMetrics
+{
+	public class RollbackService: PipelineService
+	{
+		protected override Core.Services.ServiceOutcome DoPipelineWork()
+		{
+			throw new NotImplementedException();
+		}
+
+		
+	}
+}
