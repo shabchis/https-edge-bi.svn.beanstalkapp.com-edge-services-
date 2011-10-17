8cf89bbee96ece6fd0db0f2a7647e9b6c7ed93ef
sbarchen <sbarchen@738af68e-eb0f-4933-9496-cee620179486>
2011-10-17 08:41:46 +0000

    added : Validation for checksum ( hasnt been tested yet )

--- Edge.Services.Microsoft.AdCenter/trunk/Services/ProcessorService.cs
+++ Edge.Services.Microsoft.AdCenter/trunk/Services/ProcessorService.cs
@@ -24,7 +24,8 @@ namespace Edge.Services.Microsoft.AdCenter
 
 		protected override ServiceOutcome DoPipelineWork()
 		{
-			// TODO: add checks for delivery state
+
+            // TODO: add checks for delivery state
 			string[] requiredHeaders;
 			requiredHeaders = new string[1];
 			requiredHeaders[0] = WS.CampaignPerformanceReportColumn.CampaignName.ToString();
@@ -34,22 +35,26 @@ namespace Edge.Services.Microsoft.AdCenter
 			DeliveryFile adReport = this.Delivery.Files[Const.Files.AdReport];
 			DeliveryFile campaignReport = this.Delivery.Files[Const.Files.CampaignReport];
 			var campaignReportReader = new CsvDynamicReader(campaignReport.OpenContents(), requiredHeaders);
-			while (campaignReportReader.Read())
-			{
-				string accountNameColVal = campaignReportReader.Current[WS.CampaignPerformanceReportColumn.AccountName.ToString()];
 
-				if (accountNameColVal.Trim() == string.Empty || accountNameColVal.Trim() == endOfFileMicrosoftCorporation.Trim())//end of file
-					break;
-				Campaign campaign = CreateCampaign(campaignReportReader.Current);
-				_campaignsCache.Add(campaign.Name, campaign);
-			}
-			this.ReportProgress(0.2);
+            #region Reading campaigns file
+            while (campaignReportReader.Read())
+            {
+                string accountNameColVal = campaignReportReader.Current[WS.CampaignPerformanceReportColumn.AccountName.ToString()];
 
+                if (accountNameColVal.Trim() == string.Empty || accountNameColVal.Trim() == endOfFileMicrosoftCorporation.Trim())//end of file
+                    break;
+                Campaign campaign = CreateCampaign(campaignReportReader.Current);
+                _campaignsCache.Add(campaign.Name, campaign);
+            }
+            this.ReportProgress(0.2); 
+            #endregion
 
 
 			using (var session = new AdMetricsImportManager(this.Instance.InstanceID))
 			{
 				session.BeginImport(this.Delivery);
+
+              
 
 				//    // ...............................................................
 				//    // Read the ad report, and build a lookup table for later
@@ -63,10 +68,9 @@ namespace Edge.Services.Microsoft.AdCenter
 				using (adReportReader)
 				{
 
-
 					while (adReportReader.Read())
 					{
-						//            // create the ad
+						// create the ad
 						string accountNameColVal = adReportReader.Current[WS.AdPerformanceReportColumn.AccountName.ToString()];//end of file
 						if (accountNameColVal.Trim() == string.Empty || accountNameColVal.Trim() == endOfFileMicrosoftCorporation.Trim())
 							break;
@@ -96,12 +100,17 @@ namespace Edge.Services.Microsoft.AdCenter
 				requiredHeaders[0] = "Keyword";
 				var keywordReportReader = new CsvDynamicReader(keywordReport.OpenContents(), requiredHeaders);
 
+                Dictionary<string, double> _totals = new Dictionary<string, double>(); //for checksum validation
+                //Added by Shay for validation 
+                foreach (KeyValuePair<string, Measure> measure in session.Measures)
+                {
+                    _totals.Add(measure.Key, 0);
+                }
+
 
 				// read and save in transaction
 				using (keywordReportReader)
 				{
-
-
 					while (keywordReportReader.Read())
 					{
 						string GregorianDateColVal = keywordReportReader.Current.GregorianDate;
@@ -110,10 +119,17 @@ namespace Edge.Services.Microsoft.AdCenter
 							break;
 						// get the unit from the keyword report, and add the missing ad data
 						AdMetricsUnit unit = CreateMetrics(keywordReportReader.Current, timePeriodColumn, session);
-						session.ImportMetrics(unit);
+                        session.ImportMetrics(unit);
 
+                        _totals[Measure.Common.Clicks] += unit.MeasureValues[session.Measures[Measure.Common.Clicks]];
+                        _totals[Measure.Common.Cost] += unit.MeasureValues[session.Measures[Measure.Common.Cost]];
+                        //_totals[Measure.Common.Impressions] += unit.MeasureValues[session.Measures[Measure.Common.Impressions]];
 
+						
 					}
+
+                    session.HistoryEntryParameters.Add(AdMetricsImportManager.Consts.DeliveryHistoryParameters.ChecksumTotals, _totals);
+                    session.EndImport();
 
 					ReportProgress(1);
 				}
@@ -123,8 +139,7 @@ namespace Edge.Services.Microsoft.AdCenter
 			return ServiceOutcome.Success;
 		}
 
-
-
+      
 		private Campaign CreateCampaign(dynamic values)
 		{
 			Campaign campaign = new Campaign()
@@ -158,7 +173,7 @@ namespace Edge.Services.Microsoft.AdCenter
 		}
 
 
-		private Ad CreateAd(dynamic values)
+        private Ad CreateAd(dynamic values)
 		{
 			Ad ad = new Ad()
 			{
@@ -187,6 +202,8 @@ namespace Edge.Services.Microsoft.AdCenter
 		{
 			//if (String.IsNullOrWhiteSpace(timePeriodColumn))
 			//    throw new ArgumentNullException("timePeriodColumn");
+           
+          
 
 			var unit = new AdMetricsUnit();
 
@@ -224,10 +241,8 @@ namespace Edge.Services.Microsoft.AdCenter
 			target.OriginalID = values[WS.KeywordPerformanceReportColumn.KeywordId.ToString()];
 
 			unit.TargetMatches.Add(target);
-			
 
-
-			
+           
 
 			return unit;
 		}
