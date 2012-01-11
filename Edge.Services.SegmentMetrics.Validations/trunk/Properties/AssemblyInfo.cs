6f90088f5a08caf5706737a7d93ac9d2ea24c6e8
sbarchen <sbarchen@738af68e-eb0f-4933-9496-cee620179486>
2012-01-11 09:43:04 +0000

    New *** SegmentMetrics

--- Edge.Services.SegmentMetrics.Validations/trunk/Properties/AssemblyInfo.cs
+++ Edge.Services.SegmentMetrics.Validations/trunk/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Edge.Services.Metrics.Validations")]
+[assembly: AssemblyTitle("Edge.Services.SegmentMetrics.Validations")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Edge.BI")]
