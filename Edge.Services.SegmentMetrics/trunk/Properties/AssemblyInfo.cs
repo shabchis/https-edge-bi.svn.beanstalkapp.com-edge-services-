a929b3f9a8d55b1486f217892040e82fd558ebbe
sbarchen <sbarchen@738af68e-eb0f-4933-9496-cee620179486>
2012-02-21 11:02:20 +0000

    Preparing for deployment

--- Edge.Services.SegmentMetrics/trunk/Properties/AssemblyInfo.cs
+++ Edge.Services.SegmentMetrics/trunk/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.9.0.513")]
-[assembly: AssemblyFileVersion("2.9.0.513")]
+[assembly: AssemblyVersion("2.9.0.529")]
+[assembly: AssemblyFileVersion("2.9.0.529")]
 
