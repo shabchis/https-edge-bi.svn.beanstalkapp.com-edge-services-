ef9571e43d097c33384a041e13b905c88d13777a
doronassayas <doronassayas@738af68e-eb0f-4933-9496-cee620179486>
2012-03-28 12:52:01 +0000

    about to merge with genericMappings branch

--- Edge.Services.AdMetrics/branches/Mapping/Services/ProcessorService.cs
+++ Edge.Services.AdMetrics/branches/Mapping/Services/ProcessorService.cs
@@ -12,12 +12,16 @@ namespace Edge.Services.AdMetrics
 {
 	public class ProcessorService: PipelineService
 	{
+		public Dictionary<string, Account> Accounts;
+		public Dictionary<string, Channel> Channels;
+		public AdMetricsImportManager ImportManager;
+
 		protected override Core.Services.ServiceOutcome DoPipelineWork()
 		{ 
 			// TODO: setup/defaults/configuration/etc.
 			// ------------------------------------------
 			
-			/*
+			
 			string fileName;
 			if (!this.Instance.Configuration.Options.TryGetValue(Const.ConfigurationOptions.DeliveryFileName, out fileName))
 				throw new ConfigurationException(String.Format("{0} is missing in the service configuration options.", Const.ConfigurationOptions.DeliveryFileName));
@@ -35,10 +39,13 @@ namespace Edge.Services.AdMetrics
 			}
 			else
 				compression = FileCompression.None;
-			*/
+			
 
 			var mappingConfig = new MappingConfiguration();
-			mappingConfig.ExternalMethods.Add("GetChannelByName", new Func<string, Channel>(channelName => new Channel() { Name = channelName }));
+
+			mappingConfig.ExternalMethods.Add("GetChannel", new Func<string, Channel>(channelName => new Channel() { Name = channelName }));
+			mappingConfig.ExternalMethods.Add("GetAccount", new Func<string, Channel>(channelName => new Channel() { Name = channelName }));
+			
 			mappingConfig.OnFieldRead = field => "chinese";
 			mappingConfig.Load("conduit-mappings.xml");
 
@@ -87,5 +94,61 @@ namespace Edge.Services.AdMetrics
 			return Core.Services.ServiceOutcome.Success;
 		}
 
+		public Account GetAccount(string name)
+		{
+			Account a;
+			if (!Accounts.TryGetValue(name, out a))
+				throw new MappingException(String.Format("No account named '{0}' could be found, or it cannot be used from within account #{1}.", name, Instance.AccountID));
+			return a;
+		}
+
+		public Channel GetChannel(string name)
+		{
+			Channel c;
+			if (!Channels.TryGetValue(name, out c))
+				throw new MappingException(String.Format("No channel named '{0}' could be found.", name));
+			return c;
+		}
+
+		public Segment GetSegment(string name)
+		{
+			Segment s;
+			if (!ImportManager.Segments.TryGetValue(name, out s))
+				throw new MappingException(String.Format("No segment named '{0}' could be found.", name));
+			return s;
+		}
+
+		public Measure GetMeasure(string name)
+		{
+			Measure m;
+			if (!ImportManager.Measures.TryGetValue(name, out m))
+				throw new MappingException(String.Format("No measure named '{0}' could be found. Make sure you specified the base measure name, not the display name.", name));
+			return m;
+		}
+
+		public SegmentValue GetAutoSegmentValue(string segment, string source)
+		{
+			return this.AutoSegments.ExtractSegmentValue(GetSegment(segment), source);
+		}
+
+		public DateTime CreatePeriod(DateTimeSpecificationAlignment align, string year, string month, string day)//, string hour = null, string minute = null, string second = null )
+		{
+			DateTime baseDateTime;
+			try { baseDateTime = new DateTime(Int32.Parse(year), Int32.Parse(month), Int32.Parse(day)); }
+			catch (Exception ex)
+			{
+				throw new MappingException(String.Format("Could not parse the date parts (y = '{0}', m = '{1}', d = '{2}'.", year, month, day), ex);
+			}
+
+			DateTime period;
+			period = new DateTimeSpecification()
+				{
+					Alignment = align,
+					BaseDateTime = baseDateTime
+				}
+				.ToDateTime();
+
+			return period;
+		}
 	}
 }
