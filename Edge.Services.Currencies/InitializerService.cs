aae52dff7f812a32852275acb65719bdab03ec0d
sbarchen <sbarchen@738af68e-eb0f-4933-9496-cee620179486>
2012-07-22 07:24:00 +0000

    New Pipeline project

---
+++ Edge.Services.Currencies/InitializerService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Edge.Data.Pipeline.Services;
+using Edge.Data.Pipeline;
+
+namespace Edge.Services.Currencies
+{
+    public class InitializerService : PipelineService
+    {
+
+        protected override Edge.Core.Services.ServiceOutcome DoPipelineWork()
+        {
+            this.Delivery = this.NewDelivery(); // setup delivery
+
+            string fileName = string.Empty;
+
+            //FileName
+            if (String.IsNullOrEmpty(this.Instance.Configuration.Options["FileName"]))
+                throw new Exception("Missing Configuration Param , FileName");
+            else
+                fileName = this.Instance.Configuration.Options["FileName"];
+
+            //CrossRateSymbols
+            if (String.IsNullOrEmpty(this.Instance.Configuration.Options["CrossRateSymbols"]))
+                throw new Exception("Missing Configuration Param , CrossRateSymbols");
+            else
+                this.Delivery.Parameters.Add("CrossRateSymbols", this.Instance.Configuration.Options["CrossRateSymbols"]);
+
+            //UserName
+            if (String.IsNullOrEmpty(this.Instance.Configuration.Options["UserName"]))
+                throw new Exception("Missing Configuration Param , UserName");
+            else
+                this.Delivery.Parameters.Add("UserName", this.Instance.Configuration.Options["UserName"]);
+
+            //UserPassword
+            if (String.IsNullOrEmpty(this.Instance.Configuration.Options["UserPassword"]))
+                throw new Exception("Missing Configuration Param , UserPassword");
+            else
+                this.Delivery.Parameters.Add("UserPassword", this.Instance.Configuration.Options["UserPassword"]);
+
+
+            DeliveryFile _file = new DeliveryFile()
+            {
+                Name = fileName
+            };
+
+            _file.SourceUrl = Instance.Configuration.Options["SourceUrl"];// "http://www.xignite.com/xCurrencies.asmx";
+            _file.Parameters.Add("Content-Type", "text/xml; charset=utf-8");
+            _file.Parameters.Add("SOAPAction", Instance.Configuration.Options["SOAPAction"]);
+            _file.Parameters.Add("SoapMethod", Instance.Configuration.Options["SoapMethod"]);
+            
+            this.Delivery.Save();
+            return Core.Services.ServiceOutcome.Success;
+        }
+    }
+}
---
+++ Edge.Services.Currencies/RetrieverService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Edge.Data.Pipeline.Services;
+using System.Net;
+using Edge.Data.Pipeline;
+
+namespace Edge.Services.Currencies
+{
+    class RetrieverService : PipelineService
+    {
+
+        protected override Core.Services.ServiceOutcome DoPipelineWork()
+        {
+
+            //XigniteCurrenciesService.XigniteCurrenciesSoap currenciesService;
+            //Header header = new Header();
+            //header.Username = this.Delivery.Parameters["UserName"].ToString();
+            //header.Password = this.Delivery.Parameters["UserPassword"].ToString();
+
+            //string symbols = this.Delivery.Parameters["CrossRateSymbols"].ToString();
+
+            //GetAverageHistoricalCrossRatesRequest request =
+            //    new GetAverageHistoricalCrossRatesRequest(header, symbols, StartDate: "07/21/2012", EndDate: "07/21/2012");
+
+
+            //var a = currenciesService.GetAverageHistoricalCrossRates(request);
+
+
+            // Create a batch and use its progress as the service's progress
+            BatchDownloadOperation batch = new BatchDownloadOperation();
+            batch.Progressed += new EventHandler((sender, e) =>
+            {
+                this.ReportProgress(batch.Progress * 0.95);
+            });
+
+            foreach (DeliveryFile file in this.Delivery.Files)
+            {
+                WebRequest request = WebRequest.Create(file.SourceUrl);
+                request.ContentType = file.Parameters["Content-Type"].ToString();
+                request.Method = "POST";
+
+                byte[] bytes = Encoding.UTF8.GetBytes(file.Parameters["Body"].ToString());
+                request.ContentLength = bytes.Length;
+
+                using (var stream = request.GetRequestStream())
+                {
+                    stream.Write(bytes, 0, bytes.Length);
+                }
+
+                //Headers
+                request.Headers.Add("SOAPAction", file.Parameters["SOAPAction"].ToString());
+
+                this.Delivery.Save();
+                DeliveryFileDownloadOperation download = file.Download(request);
+                //download.Ended += new EventHandler(download_Ended);
+                batch.Add(download);
+            }
+
+            batch.Start();
+            batch.Wait();
+
+            this.Delivery.Save();
+            return Core.Services.ServiceOutcome.Success;
+        }
+    }
+}
