7c294b0203749688b440b788ef4393582474145f
sbarchen <sbarchen@738af68e-eb0f-4933-9496-cee620179486>
2012-12-30 09:17:27 +0000

    Bug FIX - removed from json deserialize - "data"

--- Edge.Services.Facebook.GraphApi/trunk/RetrieverService.cs
+++ Edge.Services.Facebook.GraphApi/trunk/RetrieverService.cs
@@ -24,7 +24,7 @@ namespace Edge.Services.Facebook.GraphApi
 			//Get Access token
 
 			_accessToken = this.Instance.Configuration.Options[FacebookConfigurationOptions.AccessToken];
-			
+
 			BatchDownloadOperation countBatch = new BatchDownloadOperation();
 
 			var toremove = from f in Delivery.Files
@@ -33,51 +33,52 @@ namespace Edge.Services.Facebook.GraphApi
 			foreach (string item in toremove.ToList())
 			{
 				Delivery.Files.Remove(item);
-				
+
 			}
-			
+
 			foreach (DeliveryFile file in Delivery.Files)
 			{
 				if (file.Parameters[Consts.DeliveryFileParameters.FileSubType].Equals((long)Consts.FileSubType.Length))
 				{
 					FileDownloadOperation fileDownloadOperation = file.Download(CreateRequest(file.Parameters[Consts.DeliveryFileParameters.Url].ToString() + "limit=0"));
-					countBatch.Add(fileDownloadOperation);	 
-				}			
+					countBatch.Add(fileDownloadOperation);
+				}
 			}
 			countBatch.Progressed += new EventHandler(counted_Batch_Progressed);
 			countBatch.Start();
 			countBatch.Wait();
 			countBatch.EnsureSuccess();
 			List<DeliveryFile> files = new List<DeliveryFile>();
-			Dictionary<Consts.FileTypes,List<string>> filesByType=new Dictionary<Consts.FileTypes,List<string>>();
+			Dictionary<Consts.FileTypes, List<string>> filesByType = new Dictionary<Consts.FileTypes, List<string>>();
 			Delivery.Parameters.Add("FilesByType", filesByType);
 
 			foreach (DeliveryFile file in Delivery.Files.Where(f => f.Parameters[Consts.DeliveryFileParameters.FileSubType].Equals((long)Consts.FileSubType.Length)))
 			{
-				using (StreamReader reader=new StreamReader(file.OpenContents()))
+				using (StreamReader reader = new StreamReader(file.OpenContents()))
 				{
 					int offset = 0;
 					int limit = 500;
-					MyType t = JsonConvert.DeserializeObject<MyType>(reader.ReadToEnd());
-					while (offset<t.count)
+					string json = reader.ReadToEnd();
+					MyType t = JsonConvert.DeserializeObject<MyType>(json);
+					while (offset < t.count)
 					{
-						DeliveryFile f= new DeliveryFile();
-						f.Name = string.Format(file.Name,offset);
+						DeliveryFile f = new DeliveryFile();
+						f.Name = string.Format(file.Name, offset);
 						f.Parameters.Add(Consts.DeliveryFileParameters.Url, string.Format("{0}&limit={1}&offset={2}", file.Parameters[Consts.DeliveryFileParameters.Url], limit, offset));
-						f.Parameters.Add(Consts.DeliveryFileParameters.FileSubType,(long) Consts.FileSubType.Data);
-						f.Parameters.Add(Consts.DeliveryFileParameters.FileType,Enum.Parse(typeof(Consts.FileTypes), file.Parameters[Consts.DeliveryFileParameters.FileType].ToString()));
+						f.Parameters.Add(Consts.DeliveryFileParameters.FileSubType, (long)Consts.FileSubType.Data);
+						f.Parameters.Add(Consts.DeliveryFileParameters.FileType, Enum.Parse(typeof(Consts.FileTypes), file.Parameters[Consts.DeliveryFileParameters.FileType].ToString()));
 						files.Add(f);
-						if (!filesByType.ContainsKey((Consts.FileTypes)f.Parameters[Consts.DeliveryFileParameters.FileType]))						
-							filesByType.Add((Consts.FileTypes)f.Parameters[Consts.DeliveryFileParameters.FileType],new List<string>());
+						if (!filesByType.ContainsKey((Consts.FileTypes)f.Parameters[Consts.DeliveryFileParameters.FileType]))
+							filesByType.Add((Consts.FileTypes)f.Parameters[Consts.DeliveryFileParameters.FileType], new List<string>());
 						filesByType[(Consts.FileTypes)f.Parameters[Consts.DeliveryFileParameters.FileType]].Add(f.Name);
-						
+
 						offset += limit;
 					}
-					offset = 0;				
+					offset = 0;
 				}
 			}
 			BatchDownloadOperation batch = new BatchDownloadOperation();
-			foreach (var file in files)			
+			foreach (var file in files)
 				this.Delivery.Files.Add(file);
 			this.Delivery.Save();
 
@@ -103,7 +104,7 @@ namespace Edge.Services.Facebook.GraphApi
 		}
 
 		private HttpWebRequest CreateRequest(string baseUrl, string[] extraParams = null)
-		{			
+		{
 			HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(string.Format("{0}&access_token={1}", baseUrl, _accessToken));
 			return request;
 		}
@@ -112,19 +113,20 @@ namespace Edge.Services.Facebook.GraphApi
 			BatchDownloadOperation batchDownloadOperation = (BatchDownloadOperation)sender;
 			this.ReportProgress(batchDownloadOperation.Progress * firstBatchRatio);
 		}
-		
+
 	}
 	public class MyType
 	{
-		public string[] data;
+		//public string[] data;
+		//public class paging
+		//{
+		//	public string next;
+		//	public string previous;
+		//}
 		public int limit;
 		public int offset;
 		public int count;
-		public class paging
-		{
-			public string next;
-			public string previous;
-		}
+		
 
 	}
 }
