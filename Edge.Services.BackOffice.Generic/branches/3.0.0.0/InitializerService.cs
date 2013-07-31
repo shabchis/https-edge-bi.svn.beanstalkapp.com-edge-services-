0ef0149c7de09f176bbeb76ce05aa97812906f6b
shirat <shirat@738af68e-eb0f-4933-9496-cee620179486>
2013-07-31 11:34:00 +0000

    Delivery Pipeline: add generic backoffice service + tests for Payoneer Backoffice

---
+++ Edge.Services.BackOffice.Generic/branches/3.0.0.0/InitializerService.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Edge.Data.Pipeline;
+using Edge.Data.Pipeline.Services;
+using Edge.Core.Services;
+using Edge.Data.Objects;
+using Edge.Data.Pipeline.Metrics.Managers;
+
+namespace Edge.Services.BackOffice.Generic
+{
+    public class InitializerService : PipelineService
+    {
+        protected override ServiceOutcome DoPipelineWork()
+        {
+            #region Init General
+			if (!Configuration.TimePeriod.HasValue)
+				throw new Exception("No time period is set for Service");
+
+			if (!Configuration.Parameters.ContainsKey("AccountID"))
+				throw new Exception("Missing Configuration Param: AccountID");
+
+			if (!Configuration.Parameters.ContainsKey(Const.DeliveryServiceConfigurationOptions.FileDirectory))
+				throw new Exception(String.Format("Missing Configuration Param: {0}", Const.DeliveryServiceConfigurationOptions.FileDirectory));
+
+			if (!Configuration.Parameters.ContainsKey(BoConfigurationOptions.BaseServiceAddress))
+				throw new Exception(String.Format("Missing Configuration Param: {0}", BoConfigurationOptions.BaseServiceAddress));
+
+			var accountId = Configuration.Parameters.Get("AccountID", false, -1);
+			var channelId = Configuration.Parameters.Get("ChannelID", false, -1);
+
+			Delivery = NewDelivery();
+			Delivery.Account = new Account { ID = accountId };
+			Delivery.Channel = new Channel { ID = channelId };
+			Delivery.TimePeriodDefinition = Configuration.TimePeriod.Value;
+			Delivery.FileDirectory = Configuration.Parameters.Get<string>(Const.DeliveryServiceConfigurationOptions.FileDirectory);
+
+			// Create an import manager that will handle rollback, if necessary
+			HandleConflicts(new MetricsDeliveryManager(InstanceID), DeliveryConflictBehavior.Abort);
+          
+            var baseAddress = Configuration.Parameters.Get<string>(BoConfigurationOptions.BaseServiceAddress);
+            
+            int utcOffset = 0;
+			if (Configuration.Parameters.ContainsKey(BoConfigurationOptions.UtcOffset))
+                utcOffset = Configuration.Parameters.Get<int>(BoConfigurationOptions.UtcOffset);
+			Delivery.Parameters.Add(BoConfigurationOptions.UtcOffset, utcOffset);
+
+			int timeZone = 0;
+			if (Configuration.Parameters.ContainsKey(BoConfigurationOptions.TimeZone))
+				timeZone = Configuration.Parameters.Get<int>(BoConfigurationOptions.TimeZone);
+			Delivery.Parameters.Add(BoConfigurationOptions.TimeZone, timeZone);
+
+			Progress = 0.2;
+            #endregion
+
+            #region DeliveryFile
+            var boFile = new DeliveryFile();
+			DateTime start = Delivery.TimePeriodDefinition.Start.ToDateTime();
+			DateTime end = Delivery.TimePeriodDefinition.End.ToDateTime();
+            boFile.Parameters[BoConfigurationOptions.BO_XPath_Trackers] = Configuration.Parameters.Get<string>(BoConfigurationOptions.BO_XPath_Trackers);
+            boFile.Name = BoConfigurationOptions.BoFileName;
+			if (utcOffset != 0)
+			{
+				start = start.AddHours(utcOffset);
+				end = end.AddHours(utcOffset);				
+			}
+			if (timeZone != 0)
+			{
+				start = start.AddHours(-timeZone);
+				end = end.AddHours(-timeZone);
+			}
+			boFile.SourceUrl = string.Format(baseAddress, start, end);
+            boFile.Parameters.Add(BoConfigurationOptions.IsAttribute, Configuration.Parameters.Get<bool>(BoConfigurationOptions.IsAttribute));
+            boFile.Parameters.Add(BoConfigurationOptions.TrackerFieldName, Configuration.Parameters.Get<string>(BoConfigurationOptions.TrackerFieldName));
+
+            this.Delivery.Files.Add(boFile);
+            this.Delivery.Save();
+
+            #endregion
+
+            return ServiceOutcome.Success;
+        }
+    }
+}
---
+++ Edge.Services.BackOffice.Generic/branches/3.0.0.0/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Edge.Services.BackOffice.Generic")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Edge.Services.BackOffice.Generic")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7a50c1b7-3fce-4e62-9be3-a497424342f5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
